360077f41b77c8e3a58d0a9eb47ffe10f5cb468c
Jonathan Lynch <priestlyd@gmail.com>
2023-09-19 17:03:47 -0400

    Added column sections, fixed emojis.

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+
+/* Constants */
+
+/*Button Text*/
+var musicOnText = "Music "+" \u{1F50A}";
+var musicOffText = "Music "+" \u{1F507}";
+var portfolioText = "Portfolio"+" \u{1F4BC}";
+var resumeText = "Resume"+" \u{1F4C3}";
+
+/* Element Initialization */
 var musicButton = document.getElementById('musicButton');
 var cvButton = document.getElementById('cvButton');
-musicButton.innerText= "Music ❌";
-cvButton.innerText= "Resume 📄";
-let playing = false;
-let songAudio = null;
+var portfolioButton = document.getElementById('portButton');
 
+musicButton.innerText= musicOffText;
+cvButton.innerText= resumeText;
+portfolioButton.innerText = portfolioText;
+
+var playing = false;
+var songAudio = null;
+
+/* Music Button */
 function playMIDI() {
     if (songAudio){
         songAudio.pause();
         songAudio = null;
-        
-        musicButton.innerText = "Music ❌";
+        musicButton.innerText = musicOffText;
         return;
     }
-    songAudio = new Audio('passport.m4a');
+    songAudio = new Audio('music/passport.m4a');
     songAudio.loop = true;
     songAudio.play();
-    musicButton.innerText= "Music "+" \u{1F50A}";
+    musicButton.innerText= musicOnText;
 }
